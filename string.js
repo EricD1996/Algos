a6088f126df1bf1952e129e72cfde0b30dd7ad79
EricD1996 <EricD8816051@gmail.com>
2022-11-21 15:33:13 -0800

    Encode Decode

--- string.js
+++ string.js
@@ -1,8 +1,8 @@
 const str1 = "hello";
-const expected1 = "olleh";
+const exp1 = "olleh";
 
 const str2 = "";
-const expected2 = "";
+const exp2 = "";
 
 function reverseString(str){
     var newString = "";
@@ -37,3 +37,59 @@ function acronymize(str) {
 }
 
 console.log(acronymize(two_str3));
+
+const string1 = "aaaabbcdddaa";
+const expected1 = "a4b2c1d3a2";
+
+const string2 = "";
+const expected2 = "";
+
+const string3 = "a";
+const expected3 = "a1";
+
+const string4 = "bbcc";
+const expected4 = "b2c2";
+
+function encodeStr(str) {
+    var count = 1;
+    var newStr = "";
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === str[i+1]){
+            count ++;
+        } else {
+            newStr += str[i];
+            newStr += count;
+            count = 1;
+        }
+    }
+    return newStr;
+}
+
+console.log(encodeStr(string1));
+
+
+const twostr1 = "a3b2c1d3";
+const twoexpected1 = "aaabbcddd";
+
+const twostr2 = "a3b2c12d10";
+const twoexpected2 = "aaaabbccccccccccccdddddddddd";
+
+function decodeStr(str){
+    let decoded = "";
+    let i = 0;
+    while(i < str.length){
+        let char = str[i++]
+        let numStr = ""
+
+        let isNumber = isNaN(parseInt(str[i])) === false
+
+        while (i < str.length && isNumber){
+            numStr += str[i++]
+            isNumber = isNaN(parseInt(str[i])) === false
+        }
+        decoded += char.repeat(numStr)
+    }
+    return decoded;
+}
+
+console.log(decodeStr(twostr2));
