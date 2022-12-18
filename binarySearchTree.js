41f48e2f9fbe78c5c17e6436660df4dae686f8af
EricD1996 <EricD8816051@gmail.com>
2022-12-18 14:01:55 -0800

    binaryTree

---
+++ binarySearchTree.js
@@ -0,0 +1,75 @@
+
+class BSTNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+
+    min(current = this.root) {
+        if(this.isEmpty()) return null;
+        while(current.left != null) current = current.left
+        return current.data;
+    }
+
+    minRecursive(current = this.root) {
+        if(this.isEmpty()) return null;
+        if(current.left != null) return this.minRecursive(current.left);
+        else return current.data;
+    }
+
+    max(current = this.root) {
+        if(this.isEmpty()) return null;
+        while(current.right != null){
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    maxRecursive(current = this.root) {
+        if(this.isEmpty()) return null;
+        if(current.right != null){
+            return this.maxRecursive(current.right);
+        } else return current.data;
+    }
+
+    print(node = this.root, spaceCnt = 0, spaceIncr = 10){
+        if(!node) {
+            return;
+        }
+        space += spaceIncr;
+        this.print(node.right, spaceCnt);
+        console.log(
+            " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) + `${node.data}`
+        );
+        this.print(node.left, spaceCnt);
+    }
+}
+
+const emptyTree = new BinarySearchTree();
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new BSTNode(10);
+
+const twoLevelTree = new BinarySearchTree();
+twoLevelTree.root = new BSTNode(10);
+twoLevelTree.root.left = new BSTNode(5);
+twoLevelTree.root.right = new BSTNode(15);
+
+const threeLevelTree = new BinarySearchTree();
+threeLevelTree.root = new BSTNode(10);
+threeLevelTree.root.left = new BSTNode(5);
+threeLevelTree.root.left.left = new BSTNode(2);
+threeLevelTree.root.left.right = new BSTNode(6);
+threeLevelTree.root.right = new BSTNode(15);
+threeLevelTree.root.right.left = new BSTNode(13);
+console.log(threeLevelTree.maxRecursive());
