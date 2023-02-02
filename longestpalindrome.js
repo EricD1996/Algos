380e1e73ad56865ddea23758b01c74f0b1aaaf49
EricD1996 <EricD8816051@gmail.com>
2023-02-01 17:21:43 -0800

    longestpalindrome

---
+++ longestpalindrome.js
@@ -0,0 +1,21 @@
+function longestPalindrome(s){
+    let str = '';
+    for (let i = 0; i < s.length; i++){
+        for(let j = 0; j < 2; j++){
+            var left = i;
+            var right = left + j;
+            while (s[left] && s[left] === s[right]){
+                left--;
+                right++;
+            }
+            if(right - left - 1 > str.length){
+                str = s.slice(left + 1, right);
+            }
+        }
+    }
+    return str;
+}
+
+let s = "babad"
+
+console.log(longestPalindrome(s))
